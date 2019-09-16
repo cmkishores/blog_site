044d8f57a11ee8bb6283368dd82a2c182bcfae59
cmkishores <cmkishores@gmail.com>
2019-09-16 12:06:14 +0530

    Add template files, made model and created views

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Post(models.Models):
+	title = models.CharField(max_length=50)
+	comment = models.TextField()
+	author = models.EmailField()
+
 # Create your models here.
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class ListingView(ListView):
+	model = Post
+	template_name = 'index.html'
+	context_object_name = 'lists_of_posts'
+
+class DetailedListView(DetailView):
+	model = Post
+	template_name = 'detailed.html'
 
 # Create your views here.
